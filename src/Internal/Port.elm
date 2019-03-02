ff0141ce334bfbd773a65ee85fe668168c45a496
Luca <lucamug@gmail.com>
2019-03-02 18:17:02 +0900

    no message

--- src/Internal/Port.elm
+++ src/Internal/Port.elm
@@ -1,4 +1,29 @@
-port module Internal.Port exposing (pageInTopArea)
+port module Internal.Port exposing
+    ( onStoreChange
+    , pageInTopArea
+    , storeCache
+    , storeFlags
+    )
+
+import Internal.Type as Type
+import Json.Encode
 
 
 port pageInTopArea : (Bool -> msg) -> Sub msg
+
+
+port onStoreChange : (Json.Encode.Value -> msg) -> Sub msg
+
+
+port storeCache : Maybe Json.Encode.Value -> Cmd msg
+
+
+storeFlags : Bool -> Cmd msg
+storeFlags nightMode =
+    let
+        json =
+            Json.Encode.object
+                [ ( "nightMode", Json.Encode.bool nightMode )
+                ]
+    in
+    storeCache (Just json)
